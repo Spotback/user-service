a6989136ad8989179910f8ca911b936bf495bec4
Dylancorbus <dylancorbus@outlook.com>
2020-04-12 14:48:02 -0700

    encrypted config

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,7 +1,12 @@
-import app from './app';
 import * as https from 'https';
 import * as fs from 'fs';
 import * as Constants from '../utils/constants';
+import * as _ from 'lodash';
+
+const secureEnv = require('secure-env');
+_.merge(process.env, secureEnv({secret: process.env.CONF_ENC_UNLOCK}));
+
+import app from './app';
 
 const httpsOptions = {
     key: fs.readFileSync(Constants.KEY_LOCATION),
