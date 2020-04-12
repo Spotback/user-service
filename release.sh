517b00f2e0be78057b8464be45597e421ce7bf2f
Dylancorbus <dylancorbus@outlook.com>
2020-04-12 14:50:17 -0700

    version 1.0.7

--- release.sh
+++ release.sh
@@ -19,7 +19,7 @@ git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker tag $USERNAME/$IMAGE:$version
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
